d0ee7b5f0a150e72b064f8e2dcbb937338b91637
alex.sharov <AskAlexSharov@gmail.com>
2019-01-25 13:31:22 +0700

    add url download support

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"image/jpeg"
 	"image/png"
 	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 )
@@ -20,36 +22,76 @@ func main() {
 	fileName1 := os.Args[1]
 	fileName2 := os.Args[2]
 
-	fmt.Printf("Diffeernce: %d%%\n", distance(fileName1, fileName2))
+	fmt.Printf("Diffeernce: %d%%\n", readAndGetDistance(fileName1, fileName2))
 }
 
-func distance(fname1, fname2 string) (distance int) {
+func readAndGetDistance(fileName1, fileName2 string) int {
+	img1, img2 := readAndDecode(fileName1), readAndDecode(fileName2)
+	return distance(img1, img2)
+}
+
+func parseUrl(rawurl string) (*url.URL, bool) {
+	parsedUrl, err := url.ParseRequestURI(rawurl)
+	if err != nil {
+		return parsedUrl, false
+	}
+	return parsedUrl, true
+}
+
+func mustGet(rawurl string) io.ReadCloser {
+	resp, err := http.Get(rawurl)
+	if err != nil {
+		panic(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Sprintf("Unexpected response from server. Status code: %d", resp.StatusCode))
+	}
+	return resp.Body
+}
+
+func readAndDecode(filePathOrUrl string) image.Image {
 	// Read
-	file1, err := os.Open(fname1)
-	defer file1.Close()
+	url1, isUrl := parseUrl(filePathOrUrl)
+
+	if isUrl {
+		body := mustGet(filePathOrUrl)
+		defer body.Close()
+		return decode(url1.Path, body)
+	}
+
+	f, err := os.Open(filePathOrUrl)
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
+	return decode(filePathOrUrl, f)
+}
 
-	file2, err := os.Open(fname2)
-	defer file2.Close()
-	if err != nil {
-		panic(err)
+func decode(fileName string, f io.Reader) image.Image {
+	var img image.Image
+	var err error
+	ext := filepath.Ext(fileName)
+
+	if ext == ".jpeg" || ext == ".jpg" {
+		img, err = jpeg.Decode(f)
+		if err != nil {
+			panic(err)
+		}
+
 	}
 
-	// Decode
-	var img1, img2 image.Image
-
-	ext1 := filepath.Ext(fname1)
-	ext2 := filepath.Ext(fname2)
-	if (ext1 == ".jpeg" || ext1 == ".jpg") && (ext2 == ".jpeg" || ext2 == ".jpg") {
-		img1, img2 = decodeJpeg(file1, file2)
+	if ext == ".png" {
+		img, err = png.Decode(f)
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	if ext1 == ".png" && ext2 == ".png" {
-		img1, img2 = decodePng(file1, file2)
-	}
+	return img
+}
 
+func distance(img1, img2 image.Image) int {
 	// Hashing
 	ahash1 := imgsim.AverageHash(img1)
 	ahash2 := imgsim.AverageHash(img2)
@@ -62,32 +104,4 @@ func distance(fname1, fname2 string) (distance int) {
 	diffDistance := imgsim.Distance(dhash1, dhash2)
 
 	return (avgDistance + diffDistance) % 128
-}
-
-func decodeJpeg(f1 io.Reader, f2 io.Reader) (image.Image, image.Image) {
-	img1, err := jpeg.Decode(f1)
-	if err != nil {
-		panic(err)
-	}
-
-	img2, err := jpeg.Decode(f2)
-	if err != nil {
-		panic(err)
-	}
-
-	return img1, img2
-}
-
-func decodePng(f1 io.Reader, f2 io.Reader) (image.Image, image.Image) {
-	img1, err := png.Decode(f1)
-	if err != nil {
-		panic(err)
-	}
-
-	img2, err := png.Decode(f2)
-	if err != nil {
-		panic(err)
-	}
-
-	return img1, img2
 }
--- main_test.go
+++ main_test.go
@@ -5,32 +5,32 @@ import "testing"
 func Test_distance(t *testing.T) {
 	tests := []struct {
 		name         string
-		fname1       string
-		fname2       string
+		file1        string
+		file2        string
 		wantDistance int
 	}{
 		{
 			name:         "Original vs Scaled Down - same",
-			fname1:       "./test-png-original.png",
-			fname2:       "./test-png-scaled-down.png",
+			file1:        "./test-png-original.png",
+			file2:        "./test-png-scaled-down.png",
 			wantDistance: 0,
 		},
 		{
 			name:         "Original vs Damaged - 3%",
-			fname1:       "./test-png-original.png",
-			fname2:       "./test-png-damaged.png",
+			file1:        "./test-png-original.png",
+			file2:        "./test-png-damaged.png",
 			wantDistance: 3,
 		},
 		{
 			name:         "Scaled Down vs Damaged - 3%",
-			fname1:       "./test-png-scaled-down.png",
-			fname2:       "./test-png-damaged.png",
+			file1:        "./test-png-scaled-down.png",
+			file2:        "./test-png-damaged.png",
 			wantDistance: 3,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotDistance := distance(tt.fname1, tt.fname2)
+			gotDistance := readAndGetDistance(tt.file1, tt.file2)
 			if gotDistance != tt.wantDistance {
 				t.Errorf("distance() gotDistance = %v, want %v", gotDistance, tt.wantDistance)
 			}
